a6f2f281cbcc2eb495335f43ab5afab4e414fc6f
Thiago <thimachal@gmail.com>
2022-11-08 21:29:53 -0300

    complete

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const form = document.getElementById('form-deposito');
 
+function validaNome(nomeCompleto){
+    
+}
+
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 })
